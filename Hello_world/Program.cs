6de5aee1b2e5d3ebdc33fc99e191159e51296d99
Giacomo_Aprea <geki.aprea99@gmail.com>
2023-03-01 15:25:57 +0100

    Fix Bug 3

--- Hello_world/Program.cs
+++ Hello_world/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string name = args.Length > 0 ? args[0] : "World";
-Console.WriteLine("Hello, " + name + "!");
+Console.WriteLine("Hello, " + name + "!!");
